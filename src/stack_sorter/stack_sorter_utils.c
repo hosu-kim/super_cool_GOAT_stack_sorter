6b3f97f417d3a2552a6bcef17c9164057f88074f
hosu-kim <hosu@outlook.cz>
2025-02-05 15:27:08 +0100

    refactor find_insert_position function to handle edge cases for min and max values

--- src/stack_sorter/stack_sorter_utils.c
+++ src/stack_sorter/stack_sorter_utils.c
@@ -6,7 +6,7 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:44:33 by hoskim            #+#    #+#             */
-/*   Updated: 2025/02/04 19:02:50 by hoskim           ###   ########.fr       */
+/*   Updated: 2025/02/05 15:26:23 by hoskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,17 @@ int	find_insert_position(t_stack *stack_a, int num)
 {
 	int		position;
 	t_node	*current_node;
+	int		min_num;
+	int		max_num;
 
+	if (!stack_a->top_node)
+		return (0);
+	min_num = get_min(stack_a);
+	max_num = get_max(stack_a);
+	if (num < min_num)
+		return (get_position(stack_a, min_num));
+	if (num > max_num)
+		return ((get_position(stack_a, max_num) + 1) % stack_a->num_of_nodes);
 	position = 0;
 	current_node = stack_a->top_node;
 	while (current_node && current_node->next)
@@ -65,7 +75,7 @@ int	find_insert_position(t_stack *stack_a, int num)
 		position++;
 		current_node = current_node->next;
 	}
-	return (position);
+	return (0);
 }
 
 int	calculate_operations(t_stack *stack_a, t_stack *stack_b, int num)
