d45cf01430b2f02ddb296c587db54fd66c65b27c
hosu-kim <hosu@outlook.cz>
2025-02-24 19:49:47 +0100

    refactor: rename functions for clarity in stack operations

--- src/stack_sorter/sort_large.c
+++ src/stack_sorter/sort_large.c
@@ -6,20 +6,20 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 02:12:57 by hoskim            #+#    #+#             */
-/*   Updated: 2025/02/23 20:07:36 by hoskim           ###   ########seoul.kr  */
+/*   Updated: 2025/02/24 19:48:42 by hoskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-static int	find_minimum_moves(int position_of_node, int stack_size)
+static int	min_moves_to_get_node(int position_of_node, int stack_size)
 {
 	if (position_of_node <= stack_size / 2)
 		return (position_of_node - 1);
 	return (stack_size - position_of_node + 1);
 }
 
-static void	handle_stack_rotation(t_stack *stack_a, int position_of_node)
+static void	move_node_to_top(t_stack *stack_a, int position_of_node)
 {
 	if (position_of_node <= stack_a->total_nodes / 2)
 		rotate_in_a(stack_a);
@@ -49,7 +49,7 @@ static void	push_numbers_to_b(t_stack *stack_a, t_stack *stack_b, t_range chunk)
 			position = get_position_of_target_num(stack_a, chunk.min);
 			if (position > stack_a->total_nodes)
 				position = stack_a->total_nodes;
-			handle_stack_rotation(stack_a, position);
+			move_node_to_top(stack_a, position);
 			rotation_count++;
 		}
 	}
@@ -63,7 +63,7 @@ static void	sort_back_to_a(t_stack *stack_a, t_stack *stack_b)
 	while (stack_b->total_nodes > 0)
 	{
 		max_position = find_position_of_maximum(stack_b);
-		moves = find_minimum_moves(max_position, stack_b->total_nodes);
+		moves = min_moves_to_get_node(max_position, stack_b->total_nodes);
 		while (moves > 0)
 		{
 			if (max_position <= stack_b->total_nodes / 2)
